d4b431cba1366e64953d0085755e3ba45f771bba
Sobhi <sobhi_aroodsyr@hotmail.com>
2020-12-20 17:46:32 +0400

    Update phone_number_textfield.dart

--- lib/view/widgets/phone_number_textfield.dart
+++ lib/view/widgets/phone_number_textfield.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_sign_up/misc/multiple_text_fields.dart';
+import 'package:test_sign_up/view/widgets/text_field_widget.dart';
 
 import 'customs.dart';
 
@@ -23,32 +24,33 @@ class _PhoneNumberTextFieldState extends State<PhoneNumberTextField> {
           Consumer<AddPhoneNumberField>(builder: (context, tf, _) {
             return ListView.builder(scrollDirection: Axis.vertical,
     shrinkWrap: true, physics: NeverScrollableScrollPhysics(), itemCount: tf.phoneNumbers.length, itemBuilder: (context, index) {
-              return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(boxShadow: [customBoxShadow]),
-                child: TextFormField(
-                  controller: _phoneNumberController,
-                  style: Theme.of(context).textTheme.bodyText1,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      hintText: 'phone number'.toUpperCase(),
-                      hintStyle: Theme.of(context).textTheme.bodyText2,
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 20.0),
-                      border: InputBorder.none),
-                  validator: (value) =>
-                      value.isEmpty ? 'Please enter your phone number' : null,
-                  onSaved: (value) => addPhoneNumber.phoneNumbers.add(value),
-                ),
-              ),
-            );
+      return TextFieldWidget(title: 'phone number', validateMessage: 'Please enter your phone number', onSave: (value) => addPhoneNumber.phoneNumbers.add(value));
+            //   return Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Container(
+            //     decoration: BoxDecoration(boxShadow: [customBoxShadow]),
+            //     child: TextFormField(
+            //       controller: _phoneNumberController,
+            //       style: Theme.of(context).textTheme.bodyText1,
+            //       keyboardType: TextInputType.number,
+            //       decoration: InputDecoration(
+            //           fillColor: Colors.white,
+            //           filled: true,
+            //           hintText: 'phone number'.toUpperCase(),
+            //           hintStyle: Theme.of(context).textTheme.bodyText2,
+            //           contentPadding: const EdgeInsets.symmetric(
+            //               horizontal: 10.0, vertical: 20.0),
+            //           border: InputBorder.none),
+            //       validator: (value) =>
+            //           value.isEmpty ? 'Please enter your phone number' : null,
+            //       onSaved: (value) => addPhoneNumber.phoneNumbers.add(value),
+            //     ),
+            //   ),
+            // );
             });
           },),
           Container(width: double.infinity, child: RaisedButton(child: Text('Add another phone number'), onPressed: () async {
-            if (addPhoneNumber.phoneNumbertextFields.length < _fieldsLimit) {
+            if (addPhoneNumber.phoneNumberTextFields.length < _fieldsLimit) {
               await addPhoneNumber.addTextField('');
             }
           }))
